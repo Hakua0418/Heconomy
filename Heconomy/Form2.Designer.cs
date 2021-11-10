ecc993899c059e37f47be7a24ae351eda57b6bff
ohuna <ohuna@DESKTOP-SNAOCV7>
2021-11-11 04:24:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Heconomy/Form2.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Heconomy
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "企業登録フォーム";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.textBox1.Location = new System.Drawing.Point(227, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label2.Location = new System.Drawing.Point(67, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "企業名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label3.Location = new System.Drawing.Point(38, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "代表者名（MCID）";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.textBox2.Location = new System.Drawing.Point(227, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(436, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.textBox3.Location = new System.Drawing.Point(227, 177);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(436, 27);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label4.Location = new System.Drawing.Point(67, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "資本金";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label5.Location = new System.Drawing.Point(38, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "業種（大分類）";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "選択して下さい",
+            "農林水産業",
+            "林業",
+            "漁業",
+            "鉱業",
+            "建設業",
+            "製造業",
+            "電気・ガス",
+            "運輸・通信業",
+            "卸売・小売・飲食業",
+            "金融・保険業",
+            "不動産業",
+            "サービス業",
+            "分類不能産業"});
+            this.comboBox1.Location = new System.Drawing.Point(227, 233);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(439, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label6.Location = new System.Drawing.Point(38, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "業種（中分類）";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(227, 280);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(439, 28);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.button1.Location = new System.Drawing.Point(484, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "簡易登録";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.button2.Location = new System.Drawing.Point(636, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "詳細登録";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F);
+            this.Name = "Form2";
+            this.Text = "企業登録フォーム";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        internal System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Heconomy/Form2.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Heconomy
+{
+    public partial class Form2 : Form
+    {
+
+        public static readonly char DirectorySeparatorChar;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = comboBox1.SelectedIndex;
+            switch (i)
+            {
+                case 0:
+                    break;
+                case 1:
+                    comboBox2.Items.Clear();
+                    StreamReader sa = new StreamReader(@"..\..\Resorce\1.csv");
+                    while (!sa.EndOfStream)
+                    {
+                        String line = sa.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 2:
+                    comboBox2.Items.Clear();
+                    StreamReader sb = new StreamReader(@"..\..\Resorce\2.csv");
+                    while (!sb.EndOfStream)
+                    {
+                        String line = sb.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 3:
+                    comboBox2.Items.Clear();
+                    StreamReader sc = new StreamReader(@"..\..\Resorce\3.csv");
+                    while (!sc.EndOfStream)
+                    {
+                        String line = sc.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 4:
+                    comboBox2.Items.Clear();
+                    StreamReader sd = new StreamReader(@"..\..\Resorce\4.csv");
+                    while (!sd.EndOfStream)
+                    {
+                        String line = sd.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 5:
+                    comboBox2.Items.Clear();
+                    StreamReader se = new StreamReader(@"..\..\Resorce\5.csv");
+                    while (!se.EndOfStream)
+                    {
+                        String line = se.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 6:
+                    comboBox2.Items.Clear();
+                    StreamReader sf = new StreamReader(@"..\..\Resorce\6.csv");
+                    while (!sf.EndOfStream)
+                    {
+                        String line = sf.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 7:
+                    comboBox2.Items.Clear();
+                    StreamReader sg = new StreamReader(@"..\..\Resorce\7.csv");
+                    while (!sg.EndOfStream)
+                    {
+                        String line = sg.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 8:
+                    comboBox2.Items.Clear();
+                    StreamReader sh = new StreamReader(@"..\..\Resorce\8.csv");
+                    while (!sh.EndOfStream)
+                    {
+                        String line = sh.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 9:
+                    comboBox2.Items.Clear();
+                    StreamReader si = new StreamReader(@"..\..\Resorce\9.csv");
+                    while (!si.EndOfStream)
+                    {
+                        String line = si.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 10:
+                    comboBox2.Items.Clear();
+                    StreamReader sj = new StreamReader(@"..\..\Resorce\10.csv");
+                    while (!sj.EndOfStream)
+                    {
+                        String line = sj.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 11:
+                    comboBox2.Items.Clear();
+                    StreamReader sk = new StreamReader(@"..\..\Resorce\11.csv");
+                    while (!sk.EndOfStream)
+                    {
+                        String line = sk.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+                case 12:
+                    comboBox2.Items.Clear();
+                    StreamReader sl = new StreamReader(@"..\..\Resorce\12.csv");
+                    while (!sl.EndOfStream)
+                    {
+                        String line = sl.ReadLine();
+                        comboBox2.Items.Add(line);
+                    }
+                    break;
+
+
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
